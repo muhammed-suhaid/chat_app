aa03f2b04c760d51b06979b15ae68b21a43e3971
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-05-03 14:25:53 +0530

    displayed the messages using stream builder

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +6,42 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('No messages found'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: false,
+          )
+          .snapshots(),
+      builder: (context, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+
+        if (chatSnapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong...'),
+          );
+        }
+
+        final loadedmessages = chatSnapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedmessages.length,
+          itemBuilder: (context, index) {
+            return Text(loadedmessages[index].data()['text']);
+          },
+        );
+      },
     );
   }
 }
